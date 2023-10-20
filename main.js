152d15edd43eb1438be948b4b832ad2147ddaf22
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-10-20 18:24:04 +0200

    Inicializamos el repaso

---
+++ main.js
@@ -0,0 +1,8 @@
+//crea un formulario que pida nombre y edad
+//trae los campos al archivo js
+// Cuando le da click al boton de enviar, muestra por consola el valor
+//Guarda dichos valores en un objeto en el localstorage
+//Ahora muestra lo que hay en el localstorage en el DOM
+
+
+const formData = document.getElementById('#formData'); // O simplemente "form"
